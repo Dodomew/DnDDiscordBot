0fc8bba8a657806923c9cd28b611c166ade94862
dodomew <dodomew@gmail.com>
2021-03-01 16:04:35 +0100

    * added command spam protection * added interfaces for monster * added monster name argument for apiGet

--- src/bot.ts
+++ src/bot.ts
@@ -5,6 +5,7 @@ require('dotenv').config();
 // Import the discord.js module
 const fs = require('fs');
 const Discord = require('discord.js');
+const cooldowns = new Discord.Collection();
 
 
 // Create an instance of a Discord client
@@ -36,6 +37,27 @@ client.on('message', message => {
         message.reply('Command not recognized');
         return;
     }
+
+    // Command spam protection : Add command to a list with an expiration on it
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(`Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command}\` command.`);
+        }
+    }
+
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
     try {
         client.commands.get(command).execute(message, args);
--- src/commands/monster.ts
+++ src/commands/monster.ts
@@ -1,13 +1,65 @@
 import { MessageEmbed } from "discord.js";
-import apiGET, { ApiResponseProps } from "../api/apiGET";
-const fetch = require('node-fetch');
+import apiGET from "../api/apiGET";
 
 interface MonsterProps {
     name: string;
     size: string;
     type: string;
-    alignment: string
+    alignment: string;
+    armor_class: number,
+    armor_desc: string,
+    hit_points: number,
+    hit_dice: string,
+    speed: {
+        walk: number
+    },
+    strength: number,
+    dexterity: number,
+    constitution: number,
+    intelligence: number,
+    wisdom: number,
+    charisma: number,
+    strength_save: null,
+    dexterity_save: null,
+    constitution_save: null,
+    intelligence_save: null,
+    wisdom_save: null,
+    charisma_save: null,
+    perception: null,
+    skills: {
+        athletics: number,
+        intimidation: number
+    },
+    damage_vulnerabilities: string,
+    damage_resistances: string,
+    damage_immunities: string,
+    condition_immunities: string,
+    senses: string,
+    languages: string,
+    challenge_rating: string,
+    actions: MonsterAttackProps[],
+    reactions: MonsterReactionProps[],
+    legendary_desc: string,
+    legendary_actions: MonsterSpecialAbilitiesProps[],
+    special_abilities: MonsterSpecialAbilitiesProps[],
+    spell_list: string[],
+    img_main: string,
 }
+
+interface MonsterBaseAbilitiesProps {
+    name: string,
+    desc: string,
+}
+
+interface MonsterAttackProps extends MonsterBaseAbilitiesProps {
+    attack_bonus: number,
+    damage_dice: string,
+    damage_bonus: number
+}
+
+interface MonsterSpecialAbilitiesProps extends MonsterBaseAbilitiesProps { }
+
+interface MonsterReactionProps extends MonsterBaseAbilitiesProps { }
 
 const trim = (str, max) => ((str.length > max) ? `${str.slice(0, max - 3)}...` : str);
 
@@ -15,7 +67,13 @@ module.exports = {
     name: 'monster',
     description: 'Get a monster',
     async execute(message, args) {
-        apiGET(`https://api.open5e.com/monsters/aatxe`).then((monster) => {
+        if (!args.length) {
+            return message.reply("You did not specify a monster.")
+        }
+
+        const monsterName = args[0];
+
+        apiGET(`https://api.open5e.com/monsters/${monsterName}`).then((monster: MonsterProps) => {
             if (monster) {
                 const embed = new MessageEmbed()
                     .setColor('#EFFF00')
@@ -29,12 +87,8 @@ module.exports = {
                 message.channel.send(embed);
             }
             else {
-                message.channel.send("Unable to fetch monster")
+                message.channel.send(`Unable to find monster: ${monsterName}`)
             }
         });
     }
 };
-
-const monster = () => {
-    console.log('i am monster');
-}
