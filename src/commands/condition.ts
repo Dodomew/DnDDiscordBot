4d85bf1c249c2b71ae04694d052018ad945144ff
dodomew <dodomew@gmail.com>
2021-03-23 20:22:16 +0100

    upped version by 1 patch added a condition command to get all slugs of conditions added condition command to help

--- src/commands/condition.ts
+++ src/commands/condition.ts
@@ -1,20 +1,26 @@
 import apiGET from "../api/apiGET";
-import {ConditionProps} from "../types/conditionTypes";
+import {ApiConditionResponse, ConditionProps} from "../types/conditionTypes";
 
 const CacheHandler = require('../utils/cacheHandler');
 const conditionCache = new CacheHandler();
 
 const BOT_MESSAGE_STATUS = {
-    "SUCCESS": "success",
+    "CONDITION_SUCCESS": "condition_success",
+    "CONDITIONS_SUCCESS": "conditions_success",
     "CONDITION_NOT_FOUND": "condition_not_found",
     "API_ERROR": "api_error"
 };
 
 const botMessageResponse = (botMessage, state, response?) => {
     switch (state) {
-        case BOT_MESSAGE_STATUS.SUCCESS:
-            if (response) {
+        case BOT_MESSAGE_STATUS.CONDITION_SUCCESS:
+            if (response.desc) {
                 botMessage.edit(response.desc);
+            }
+            break;
+        case BOT_MESSAGE_STATUS.CONDITIONS_SUCCESS:
+            if (response.embed) {
+                botMessage.edit('Conditions:', { embed: response.embed});
             }
             break;
         case BOT_MESSAGE_STATUS.API_ERROR:
@@ -32,18 +38,43 @@ module.exports = {
     name: 'condition',
     description: 'Get info about a condition',
     async execute(message, args) {
+        const botMessage = await message.channel.send("fetching...");
+
         if (!args.length) {
-            return message.reply("You did not specify a condition.")
+            apiGET(`https://api.open5e.com/conditions/`).then((conditions: ApiConditionResponse) => {
+                
+                if (conditions) {
+                    conditionCache.set("conditions", conditions);
+                    const allConditions = conditions.results.map((condition) => {
+                        return condition.slug;
+                    });
+
+                    const embed = {
+                        color: 0x0099ff,
+                        title: 'DnD bot',
+                        url: 'https://github.com/Dodomew/DnDDiscordBot',
+                        fields: [{ name: "Conditions", value: allConditions.join(", ")}],
+                    };
+
+                    botMessageResponse(botMessage, BOT_MESSAGE_STATUS.CONDITIONS_SUCCESS, { embed: embed });
+                }
+                else {
+                    botMessageResponse(botMessage, BOT_MESSAGE_STATUS.CONDITION_NOT_FOUND);
+                }
+            }).catch((error) => {
+                botMessageResponse(botMessage, BOT_MESSAGE_STATUS.API_ERROR, { error: error });
+            });
+            
+            return;
         }
 
         const conditionName = args[0];
-        const botMessage = await message.channel.send("fetching...");
 
         if (conditionCache.has(conditionName)) {
             try {
                 if (!conditionCache.isExpired(conditionName)) {
                     const condition = conditionCache.get(conditionName);
-                    botMessageResponse(botMessage, BOT_MESSAGE_STATUS.SUCCESS, condition);
+                    botMessageResponse(botMessage, BOT_MESSAGE_STATUS.CONDITION_SUCCESS, condition);
                     return;
                 }
                 else {
@@ -58,7 +89,7 @@ module.exports = {
         apiGET(`https://api.open5e.com/conditions/${conditionName}`).then((condition: ConditionProps) => {
             if (condition) {
                 conditionCache.set(conditionName, condition);
-                botMessageResponse(botMessage, BOT_MESSAGE_STATUS.SUCCESS, condition);
+                botMessageResponse(botMessage, BOT_MESSAGE_STATUS.CONDITION_SUCCESS, condition);
             }
             else {
                 botMessageResponse(botMessage, BOT_MESSAGE_STATUS.CONDITION_NOT_FOUND, { conditionName: conditionName });
--- src/commands/help.ts
+++ src/commands/help.ts
@@ -15,6 +15,10 @@ module.exports = {
                     name: '!monster [name]',
                     value: 'Get complete info about a monster. If a name has spaces in it, replace them with hyphens, e.g. !monster goblin, !monster pit-fiend',
                 },
+                {
+                    name: '!condition [name]',
+                    value: 'Get complete info about a condition. Type **!condition** to get the names of all conditions and type **!condition [name]** to get details about a condition, e.g. !condition deafened'
+                },
             ],
         };
 
